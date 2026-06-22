de0924c5dd03722908e3ad013c6150a10c5da17c
RigAgravanshi <rigagravanshi111@gmail.com>
2026-06-22 22:22:55 +0530

    update configs

--- src/retriever.py
+++ src/retriever.py
@@ -5,6 +5,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.config_loader import CFG
 from sentence_transformers import SentenceTransformer
+from src.parser import parse_intent
 # from langchain_community.vectorstores import FAISS
 # from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
@@ -22,7 +23,7 @@ model = SentenceTransformer(CFG['indexing']['model_name'], model_kwargs={"token"
 #     embeddings=embedding_model,
 #     allow_dangerous_deserialization=True)
 
-def retrieve(prompt:str, k:int = 200):
+def retrieve(prompt:str, k:int = 200) -> pd.DataFrame:
 	# BGE model requires prefix to signal it to work with the prompt
 	prefixed = "Represent this sentence for searching relevant passages: " + prompt
 
@@ -42,7 +43,34 @@ def retrieve(prompt:str, k:int = 200):
 	# 	rows.append(row)
 	# return pd.DataFrame(rows)
 
-test_prompt = "A classy, slow Italian playlist with mafia-vibes for deep thinking"
-results = retrieve(test_prompt, k=50)
-print(results[['track_name', 'artist_name', 'genre', 'energy', 'instrumentalness', 'valence', 'similarity_score']])
-print(f"Time taken: {time.time()-start}")
+def filter_songs(songs: pd.DataFrame, intent: dict) -> pd.DataFrame:
+	filtered = songs.copy()
+	playlist_length = intent.get("playlist_length", 15) # 15  was given as a default to LLM itself
+
+	# filtering rows based on values
+	for feature in ['energy', 'valence', 'acousticness', 'instrumentalness', 'speechiness', 'danceability']:
+		if feature in intent:
+			low, high = intent[feature]
+			before = len(filtered)
+			actual_low = songs[feature].quantile(low)
+			actual_high = songs[feature].quantile(high)
+			filtered = filtered[filtered[feature].between(actual_low, actual_high)]
+			print(f"{feature} [{low}, {high}]: {before} -> {len(filtered)}")
+
+	if len(filtered) < playlist_length:
+		print(f"[WARN] Only {len(filtered)} songs after filtering as per Intent. Returning unfiltered Songs.")
+		return songs
+
+	return filtered
+
+test_prompt = "rainy evening slow melancholic 10 songs to enjoy"
+results = retrieve(test_prompt, k=500)
+# print(results[['track_name', 'artist_name', 'genre', 'energy', 'instrumentalness', 'valence', 'similarity_score']])
+
+intent_dict = parse_intent(test_prompt)
+print(intent_dict)
+filtered = filter_songs(results, intent_dict)
+print(filtered[['track_name', 'artist_name', 'energy', 'valence', 'similarity_score']])
+print(f"Songs: {len(results)} --> Filtered: {len(filtered)}")
+
+print(f"\n\nTime taken: {time.time()-start} \n\n")
